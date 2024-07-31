f9aec8c1329cfb8562391c835c14592f43ee9f8f
M9J <mnsh.exe@gmail.com>
2024-07-31 15:09:30 +0000

    Deploying to build from @ M9J/webpack-typescript-template@c15718f83ef687120a86e66833984cd1e7a4d9d0 🚀

--- index.bundle.js
+++ index.bundle.js
@@ -1,1 +1,1 @@
-(()=>{"use strict";const e="(Webpack Typescript Template) index.ts: Hello World";console.log(e);const t=document.createElement("div");t.innerHTML=e,document.body.appendChild(t)})();
+(()=>{"use strict";const e="(Webpack Typescript Template) index.ts: Hello World";console.log(e),function(e){const n=document.createElement("div");n.innerHTML=e,document.body.appendChild(n)}(e)})();
